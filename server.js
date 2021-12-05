4b5c98642dfeaaad237c47a3376ce8df736d6e73
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2021-12-06 00:15:00 +0600

    make static file path

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set("view engine", "ejs");
 // middleware & static files
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const blogs = [
   {
